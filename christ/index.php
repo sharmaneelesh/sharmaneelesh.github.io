2d056ae1793fa9a90897cb0013177a36c49beb3d
Neelesh Sharma <71886247+sharmaneelesh@users.noreply.github.com>
2022-11-18 10:48:09 +0000

    Digital Notice Board

---
+++ christ/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Department of Computer Science</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link href="index.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+<div class="container-fluid" id="section1">
+    <div class="row" id="header">
+        <div class="col-md-9" id="headertext">
+            <h2>Department of Computer Science</h2>
+            <div class="motto">
+                <div>VISION : Excellence and Service</div><div>MISSION : To develop IT professionals with ethical and human values</div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <img src="media/Christ University Black.png" id="logo">
+        </div>
+    </div>
+
+    <div class="row" id="content">
+        <div class="col-md-4">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10" id="opportunity">
+                        <h3>OPPORTUNITIES</h3>
+                        <marquee direction="up" scrollamount='4'>
+                            <ul>
+                                <li>PLACEMENTS: Simplify3X Hiring Process for MCA, BCA (Fast Track) (Saturday, November 19, 2022)</li>
+                                <li>PLACEMENTS: Trivium E Solutions Hiring Process for MCA (November 18-19, 2022)</li>
+                                <li>COMPETITION: Web Design Hackathon - Perception by Labyrinth Stack Domini (Saturday, November 19, 2022)</li>
+                            </ul>
+                        </marquee>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <img src="media/placement.png" class="placement">
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            
+            <img src="media/1.png" class="poster">
+            <img src="media/2.png" class="poster">
+            <img src="media/3.png" class="poster">
+            <img src="media/4.png" class="poster">
+            <img src="media/5.png" class="poster">
+              
+        </div>
+        <div class="col-md-4">
+            <iframe src="https://calendar.google.com/calendar/embed?src=cs%40christuniversity.in&ctz=Asia%2FKolkata&amp;mode=AGENDA&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showNav=0&amp;showTz=0&amp;showTabs" frameborder="0" scrolling="no"></iframe>
+        </div>
+    </div>
+
+    <div class="row" id="footer">
+        <div class="col-md-12">
+            <b>Digital Notice Board (v1.0)</b> &nbsp;  | &nbsp;  <b>Concept and Design:</b> Dr. Ashok Immanuel V  &nbsp; | &nbsp;  <b>Software:</b> Neelesh Sharma (2040176), Nayan L. Badolla (2040147)
+        </div>
+    </div>
+
+</div>
+
+    <script src="slideshow.js"></script>
+</body>
+</html>
